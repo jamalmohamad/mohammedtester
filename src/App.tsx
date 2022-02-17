22f669318a26785095d9bf7273213f54444edfc7
jamalmohamad <mohammed.jamalhadi@gmail.com>
2022-02-17 13:00:15 +0100

    feat: initial commit react

--- src/App.tsx
+++ src/App.tsx
@@ -6,18 +6,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Mohammed Tester</h1>
+        <p>Site Under-Construction</p>
+        <p>Contact: mohammed.jamalhadi@gmail.com</p>
       </header>
     </div>
   );
