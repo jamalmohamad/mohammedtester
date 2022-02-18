bdfc78730dc7d5aabbfbd4d2e31db51d25af2f65
MO <mohammed.jamalhadi@gmail.com>
2022-02-18 02:48:55 +0100

    Feat main page (#2)
    * feat: design main page
    
    * feat: make beautiful design

---
+++ src/components/home.tsx
@@ -0,0 +1,12 @@
+import { Button } from "react-bootstrap";
+import { Carousal } from "../utils/carousal";
+import { Introduction } from "../utils/introduction";
+
+export function Home() {
+    return (
+        <div className="">
+            <Introduction></Introduction>
+        </div>
+    );
+}
+
