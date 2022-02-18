e50330773440905000aca6490ba43b3662b1b8bb
jamalmohamad <mohammed.jamalhadi@gmail.com>
2022-02-18 10:53:32 +0100

    fix: some css

--- src/App.tsx
+++ src/App.tsx
@@ -21,11 +21,11 @@ function App() {
       
     </Nav>
     <Nav>
-      {/* <Nav.Link href="#deets">Home</Nav.Link> */}
+       <Nav.Link href="#deets">Home</Nav.Link>
       {/* <Nav.Link href="#pricing">Automation Testing</Nav.Link> */}
-      {/* <Nav.Link href="#resume">Resume</Nav.Link>
-      <Nav.Link href="#about">About</Nav.Link> */}
-      {/* <Nav.Link href="#contact">Contact</Nav.Link> */}
+       <Nav.Link href="#resume">Resume</Nav.Link>
+      {/* <Nav.Link href="#about">About</Nav.Link> */}
+       {/* <Nav.Link href="#contact">Contact</Nav.Link>  */}
     </Nav>
   </Navbar.Collapse>
   </Container>
--- src/utils/introduction.tsx
+++ src/utils/introduction.tsx
@@ -8,14 +8,12 @@ export function Introduction() {
 
                <div className="col-md-12 cover-bg">
                <div className="mycontainer">
-                    <h6>Hello and Welcome, My Name is,</h6>
-                    <h2>Mohammed</h2>
-                    <br></br>
-                    <br></br>
+                    {/* <h6>Hello and Welcome, My Name is,</h6> */}
+                    <h2>Mohammed Alhayaj</h2>
+                    {/* <br></br> */}
                     <h6>Software Developer  |  Automation Tester  |  Computer Scientist</h6>
                     </div>                 
                 </div>
-
             </div>
 
             <div className="row">
